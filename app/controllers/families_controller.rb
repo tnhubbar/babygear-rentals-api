f8c99375b006c4847d3c57bf1d2a6aa066cf2525
tnhubbar <taryn.johnson919@gmail.com>
2020-10-08 09:20:57 -0400

    added controller actions

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,2 +1,39 @@
 class FamiliesController < ApplicationController
+
+    def index
+        @accounts = Account.all 
+        render json: @accounts  
+
+    end 
+
+
+    def create 
+        @account = Account.new(account_params)
+        if @account.save 
+            render json: @account
+        else 
+            render json: {error: 'Error creating account'}
+        end 
+    end 
+
+
+    def show 
+        @account = Account.find(params[:id])
+        render json: @account 
+
+    end 
+
+    def destroy 
+        @account = Account.find(params[:id])
+        account.destroy 
+    end
+
+
+    private 
+
+    def family_params
+        params.require(:family).permit(:name, :location, :contact_number)
+
+    end 
+
 end
--- app/controllers/rental_items_controller.rb
+++ app/controllers/rental_items_controller.rb
@@ -1,2 +1,46 @@
 class RentalItemsController < ApplicationController
+
+    def index
+
+        @rental_items = Rental_item.all 
+        render json: @rental_items  
+
+    end 
+
+
+    def create 
+        @rental_item = Rental_item.new(rental_item_params)
+        if @rental_item.save 
+            render json: @rental_item
+        else 
+            render json: {error: 'Error creating rental item'}
+        end 
+    end 
+
+
+    def show 
+        @rental_item = Rental_item.find(params[:id])
+        render json: @rental_item 
+
+    end 
+
+    def destroy 
+        @rental_item = Rental_item.find(params[:id])
+        account.destroy 
+    end
+
+
+    private 
+
+    def set_family
+        @family = Family.find(params[:family_id])
+    end 
+
+    def rental_item_params
+        params.require(:rental_item).permit(:family_id, :age, :category, :item_name, :status)
+
+    end 
+
+
+
 end
