117d72dbcb4c0e9334e8658592f3acc4da1391ab
tnhubbar <taryn.johnson919@gmail.com>
2020-10-11 20:05:06 -0400

    adding material

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,13 @@
 
 family = Family.create(name: "Johnson Family", location: "Jacksonville, FL", contact_number: "843-735-2096")
 
-rental_item1 = RentalItem.create(status: "available", item_name: "stroller", family_id: "1")
-rental_item2 = RentalItem.create(status: "unavailable", item_name: "crib", family_id: "1")
+rental_item1 = RentalItem.create(status: "available", category: "Transport Items", item_name: "Stroller", family_id: "1")
+rental_item2 = RentalItem.create(status: "available", item_name: "Black Crib", family_id: "1", category: "Furniture")
+rental_item3 = RentalItem.create(status: "available", item_name: "High Chair", family_id: "1", category: "Furniture")
+
+family2 = Family.create(name: "Scottsdale Family", location: "Boston, MA", contact_number: "857-294-8321")
+
+rental_item4 = RentalItem.create(status: "available", category: "Toys", item_name: "Lego Set", family_id: "2")
+rental_item5 = RentalItem.create(status: "available", item_name: "Baby Swing", family_id: "2", category: "Toys")
+rental_item6 = RentalItem.create(status: "available", item_name: "Baby Dresser", family_id: "2", category: "Furniture")
+
